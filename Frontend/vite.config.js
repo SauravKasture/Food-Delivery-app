04093fddebd9ebb4466952a68c61a73d1fbbcdd3
Saurav Kasture <sauravkasture5777@gmail.com>
2025-03-06 15:35:32 +0530

    Configure frontend for Render and Vercel

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,21 +1,15 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: "https://food-delivery-app-wbdz.onrender.com", // Replace with your backend URL
-//         changeOrigin: true,
-//         secure: false,
-//       },
-//     },
-//   },
-// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://mern-backend-4k19.onrender.com", // Replace with your backend URL
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
