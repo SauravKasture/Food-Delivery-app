7d0805c56e7833f441fde081eb2290466dd55cfb
Saurav Kasture <sauravkasture5777@gmail.com>
2025-03-06 11:37:45 +0530

    Configure frontend for Render and Vercel

--- food-delivery-backend/server.js
+++ food-delivery-backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173" || "https://food-delivery-app-lac-xi.vercel.app/" , // Use Vercel URL in production
+  origin: process.env.FRONTEND_URL || "http://localhost:5173", // Use Vercel URL in production
   credentials: true,
 })); // Enable Cross-Origin Resource Sharing
 app.use(express.json()); // Parse JSON requests
